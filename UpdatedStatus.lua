e6518eb103b04156fd13718fdaf256d358ca9c91
Easy lua <staskuzmin1406@gmail.com>
2024-11-13 15:27:54 +0300

    Update UpdatedStatus.lua

--- UpdatedStatus.lua
+++ UpdatedStatus.lua
@@ -1,8 +1,17 @@
-if workspace:FindFirstChild("UpdateStatus") then
-workspace.UpdateStatus:Destroy()
-  else
-  local Updated = Instance.new("BoolValue")
-  Updated.Name = "UpdateStatus"
-  Updated.Parent = workspace
-  Updated.Value = false
+local folderofupdate = Instance.new("Folder")
+folderofupdate.Parent = workspace
+folderofupdate.Name "UpdateFolder"
+if workspace.UpdateFolder.UpdatedStatus.Value == true then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/King-legacy-project/refs/heads/main/Souce.lua"))()
+else
+  for _, v in pairs(workspace.UpdateFolder:GetDescendants()) do
+    if v.Name == "UpdatedStatus" then
+      v:Destroy()
+    else
+      local Update = Instance.new("BoolValue")
+      Update.Name = "UpdateStatus"
+      Update.Parent = workspace
+      Update.Value = false
+    end
+  end
 end
