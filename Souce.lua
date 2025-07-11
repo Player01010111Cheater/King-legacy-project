74cde78f616d15529c7eabf5215df66f1a222218
Easy lua <staskuzmin1406@gmail.com>
2025-07-11 13:12:21 +0300

    Update Souce.lua

--- Souce.lua
+++ Souce.lua
@@ -1,9 +1,9 @@
-local _cdefghijklmnopqrstuvcdefklmnopqrstuvwxyz=function(_mnopqrstuvwxyz,_ghijklmnopqrstrstuvwx,_cdefghijklmnopqrstuvwxy)
+local _efghijklmnopqrstu=function(_,_efghijklmnopqrstuvlmnopqrstuvw,_nopqpqrstuqrstuvw)
 local r={}
-for i=1,#_mnopqrstuvwxyz do
-r[i]=string.char((string.byte(_mnopqrstuvwxyz,i)-(_ghijklmnopqrstrstuvwx-_cdefghijklmnopqrstuvwxy)-i)%256)
+for i=1,#_ do
+r[i]=_G.string.char((_G.string.byte(_,i)-(_efghijklmnopqrstuvlmnopqrstuvw-_nopqpqrstuqrstuvw)-i)%256)
 end
 return table.concat(r)
 end
-return loadstring("    wxyzfhijklmnopqbcdefghijkl hijklmnopqrstuvwxrstuvwx = _rstuvwxyzfghijklmnopqhijklmnopqrstuv(string.char(83,72,145,147,149,151,153,166,168,170,82),48,10)\
-    print(hijklmnopqrstuvwxrstuvwx)")()
+return loadstring("    rstuvwxdefghijklmnopqrstuvwxyzdefghijkabcdefghijklmnopqrstuvwx pqrstuvwxyfghijklmntuvwx = _rstuvhijklmnopqrstuvwxyz(_G.string.char(121,123,125,127,129,131,133,135,137,128,130,132,134,136,138,140,142,144,146,148,152,154,156,158,160,151,153,155,157,159,161,163,165,167,169,171,96,85,158,160,162,164,166,168,170,172,174,176,178,180,182,184,186,166,168,182,182,184,186,188,190,192,194,196,113),21,6)\
+    print(pqrstuvwxyfghijklmntuvwx)")()
