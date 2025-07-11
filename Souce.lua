c32be259bccc2215dca04d2bb5aca7448d347061
Easy lua <staskuzmin1406@gmail.com>
2025-07-11 13:08:51 +0300

    Update Souce.lua

--- Souce.lua
+++ Souce.lua
@@ -1,14 +1,9 @@
-local _hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSvwxopqrstuvwxyzABCDEFGHIJKLMNOPQRbcdefghijklmnopqrstabcdefghijklmnopqrstuvwxyzA = function(_klmnopqrstuvwxyzABCDEFGHIJKLMNOPcdefg, _JKLjklmnopqrstuvwxyzA, _)
-    local _ABCDEFGHIJKLMNOPabcdefghijklmnopqrstuvwxyzABCDEFGH = {}
-    local _HIJKLMNOPQRSTxB = math.floor(_JKLjklmnopqrstuvwxyzA / 2) * 2
-    for _EFGHIJzABCDEFGHIJKLMNO = 1, #_klmnopqrstuvwxyzABCDEFGHIJKLMNOPcdefg do
-        local _TUVWXKLMNOPQRSTuvwxyzABC = string.byte(_klmnopqrstuvwxyzABCDEFGHIJKLMNOPcdefg, _EFGHIJzABCDEFGHIJKLMNO)
-        local _vwxyzABCDJKLMNOPQRSTab = (_TUVWXKLMNOPQRSTuvwxyzABC - (_JKLjklmnopqrstuvwxyzA - _) - _EFGHIJzABCDEFGHIJKLMNO * 2) % 256
-        _ABCDEFGHIJKLMNOPabcdefghijklmnopqrstuvwxyzABCDEFGH[_EFGHIJzABCDEFGHIJKLMNO] = string.char(_DEFGrstuvwxyzABCDEFGH)
-        if _EFGHIJzABCDEFGHIJKLMNO % 3 == 0 then local _nopqrstuvwxyzABCDmnopqrstuvwxyzABCDEFGHIJKLMNOPQRS = math.random(1, 10) end
-    end
-    return table.concat(_ABCDEFGHIJKLMNOPabcdefghijklmnopqrstuvwxyzABCDEFGH)
+local _abcdefghstuvwxyzbcdefghijk=function(_efghijklmnopqrstuvw,_vw,_opqr)
+local r={}
+for i=1,#_efghijklmnopqrstuvw do
+r[i]=string.char((string.byte(_efghijklmnopqrstuvw,i)-(_vw-_opqr)-i)%256)
 end
-return loadstring("    nopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXopqrstuvwxyzABCDEFGHIJKLMNqrstuvwxyzABCDEFGHIJKLMNOJKLMGHIJKLMNOPQRSTUVWXGHIJKLMNn pqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWnopqrstuvwxyzABCpqrstuvwxyzABCDEFGHEFGlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ = _WXYyzABCDEFGHIJKLSTUVST(string.char(226,229,232,235,238,241,244,247,250,253,0,3,6,9,210,213,216,219,222,225,228,231,234,237,240,21,24,27,30,33,36,39,42,45,48,51,54,57,2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59,62,65,68,71,96,99,102,105,108,111,114,117,120,123,126,129,74,77,80,83,86,89,92,95,98,101,104,107,110,113,138,141,144,147,150,153,156,159,162,165,168,171,174,177,180,183,186,189,192,195,198,201,146,149,152,155,158,161,164,167,170,173,176,179,203,206,209,212,210,213,216,219,222,225,228,231,234,237,240,243,246,249,252,255,2,5,8,11,14,17,20,23,26,29,230,233,236,239,242,245,248,251,9,12,34,253,0,3,6,9,12,15,18,21,24,27,30,33,37,40,43,46,49,52,55,58,61,19,9,77,80,83,86,89,92,95,97,100,103,106,109,112,115,118,121,124,127,130,133,136,139,142,145,148,151,96,99,102,105,108,111,114,117,120,123,126,129,132,135,138,141,185,130,133,136,139,142,145,148,151,154,157,160,163,166,169,208,211,214,217,220,223,226,229,174,177,180,183,186,189,192,195,198,201,204,207,210,213,3,204,207,210,213,213,216,219,222,225,228,231,234,237,240,243,246,249,252,255,2,5,8,11,251,254,1,4,7,10,13,16,19,22,25,28,31,244), 153, 38)\
-    print(pqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWnopqrstuvwxyzABCpqrstuvwxyzABCDEFGHEFGlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ)\
-local hijklmzABCDEFGHIJKLMNOPQRxyzABCDEFGHIJKL = 192;")()
+return table.concat(r)
+end
+return loadstring("    lmnopqrstuvabcdefghijklmnopabcd hijklmnopqrstubcdefghiabcdefghij = _abcdefghijklmnopqrst(string.char(131,133,135,137,124,126,128,130,132,134,136,132,134,136,138,140,142,144,146,148,150,152,144,146,148,150,152,154,156,158,160,162,164,166,168,170,172,174,176,178,180,108,97,176,181,183,185,187,189,191,193,195,180,182,184,186,188,190,192,194,196,198,200,119),42,20)\
+    print(hijklmnopqrstubcdefghiabcdefghij)")()
